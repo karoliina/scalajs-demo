6d442807e84e2224a2512bbf1b826cd5db836e8b
Karoliina Oksanen <hkoksanen@gmail.com>
2018-02-22 18:50:46 -0500

    Set up server to serve compiled JS bundles

--- cafeteria/server/app/com/citydank/cafeteria/controllers/Application.scala
+++ cafeteria/server/app/com/citydank/cafeteria/controllers/Application.scala
@@ -8,8 +8,15 @@ import play.api.mvc._
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  def bundleUrl(projectName: String, bundleSuffix: String): Option[String] = {
+    val name = projectName.toLowerCase
+    Seq(s"$name-opt$bundleSuffix.js", s"$name-fastopt$bundleSuffix.js")
+      .find(name => getClass.getResource(s"/public/$name") != null)
+      .map(controllers.routes.Assets.versioned(_).url)
+  }
+
   def index = Action {
-    Ok(views.html.index(SharedMessages.itWorks))
+    Ok(views.html.index("CityDank Cafeteria", bundleUrl("client", ""), bundleUrl("client", "-loader"), bundleUrl("client", "-library")))
   }
 
 }
