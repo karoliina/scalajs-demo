1c843031a72a4cba750bb009817f30e4b21a5456
Karoliina Oksanen <hkoksanen@gmail.com>
2018-02-24 11:48:24 -0500

    Client, step 1

--- cafeteria/client/src/main/scala/com/citydank/cafeteria/ScalaJSExample.scala
+++ cafeteria/client/src/main/scala/com/citydank/cafeteria/ScalaJSExample.scala
@@ -1,10 +1,24 @@
 package com.citydank.cafeteria
 
 import org.scalajs.dom
+import org.scalajs.dom.html
+import scala.scalajs.js
+
+import scalatags.JsDom.all._
+import autowire._
+
+import com.citydank.cafeteria.shared._
+import com.citydank.cafeteria.shared.models._
 
 object ScalaJSExample {
-
   def main(args: Array[String]): Unit = {
-    dom.document.getElementById("root").textContent = "CityDank cafeteria"
+    val root = dom.document.getElementById("root")
+
+    val content = div(
+      cls := "mdc-typography",
+      h1("CityDank Cafeteria")
+    )
+
+    root.appendChild(content.render)
   }
 }
